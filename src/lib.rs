021eda2c875b70f605feb2ab0bda73b035f14d63
FreeMasen <RFM@Herman.local>
2017-11-05 22:12:38 -0600

    add some docs comments

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,17 @@
+/// This function returns a string at least as long as the
+/// `bytes` parameter. The `numbered` will prefix each iteration
+/// of lorem ipsum.
 pub fn get_lorem(bytes: usize, numbered: bool) -> String {
     let lorem = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
     get_string(bytes, lorem, numbered)
 }
 
+/// This function returns a string at least as long as the
+/// `bytes` parameter. The `template` `&str` will be repeated
+/// the number of times required to reach the `bytes`
+/// exceeding the value if it doesn't evenly divide into the `bytes`
+/// if `numbered` is true, each iteration will be extened by the number
+/// plus '.' and a space (this will increase the overflow)
 pub fn get_string(bytes: usize, template: &str, numbered: bool) -> String {
     let mut ret = String::new();
     if numbered {
